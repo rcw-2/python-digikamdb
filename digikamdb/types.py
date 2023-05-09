8d027f67108893e3c7aa0ebd9ae9855a6220509f
Ralph <91877177+rcw-2@users.noreply.github.com>
2023-05-09 19:19:39 +0200

    More reader-friendly string conversions for Exif types

--- digikamdb/types.py
+++ digikamdb/types.py
@@ -5,6 +5,16 @@ from enum import IntEnum
 from typing import Any, Mapping, Union
 
 
+class ExifEnum(IntEnum):
+    """
+    Adds more readable string representations to Exif types.
+    
+    .. versionadded:: 0.3.0
+    """
+    def __str__(self) -> str:
+        return self.name.capitalize().replace('_', ' ')
+
+
 class AlbumRootStatus(IntEnum):
     """Class for :attr:`AlbumRoot.status<_sqla.AlbumRoot.status>`"""
     LocationAvailable   = 0
@@ -54,7 +64,7 @@ class ImageColorModel(IntEnum):
     COLORMODELRAW       = 8
 
 
-class ExifOrientation(IntEnum):
+class ExifOrientation(ExifEnum):
     """
     Exif ImageOrientation Tag
     
@@ -63,6 +73,9 @@ class ExifOrientation(IntEnum):
     will usually not show up in digital photos.
     
     Used by :attr:`ImageInformation.orientation<_sqla.ImageInformation.orientation>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """
     #: Landscape, camera held upright
     TOP_LEFT        = 1
@@ -82,22 +95,28 @@ class ExifOrientation(IntEnum):
     LEFT_BOTTOM     = 8
 
 
-class ExifExposureMode(IntEnum):
+class ExifExposureMode(ExifEnum):
     """
     Exif ExposureMode Tag
     
     Used by :attr:`ImageMetadata.exposureMode<_sqla.ImageMetadata.exposureMode>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """
     AUTO_EXPOSURE   = 0
     MANUAL_EXPOSURE = 1
     AUTO_BRACKET    = 2
 
 
-class ExifExposureProgram(IntEnum):
+class ExifExposureProgram(ExifEnum):
     """
     Exif ExposureProgram Tag
     
     Used by :attr:`ImageMetadata.exposureProgram<_sqla.ImageMetadata.exposureProgram>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """
     NOT_DEFINED         = 0
     MANUAL              = 1
@@ -110,21 +129,27 @@ class ExifExposureProgram(IntEnum):
     LANDSCAPE_MODE      = 8
 
 
-class ExifWhiteBalance(IntEnum):
+class ExifWhiteBalance(ExifEnum):
     """
     Exif WhiteBalance Tag
     
     Used by :attr:`ImageMetadata.whiteBalance<_sqla.ImageMetadata.whiteBalance>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """
     AUTO    = 0
     MANUAL  = 1
 
 
-class ExifMeteringMode(IntEnum):
+class ExifMeteringMode(ExifEnum):
     """
     Exif MeteringMode Tag
     
     Used by :attr:`ImageMetadata.meteringMode<_sqla.ImageMetadata.meteringMode>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """
     UNKNOWN                 = 0
     AVERAGE                 = 1
@@ -136,12 +161,15 @@ class ExifMeteringMode(IntEnum):
     OTHER                   = 255
 
 
-class ExifSubjectDistanceRange(IntEnum):
+class ExifSubjectDistanceRange(ExifEnum):
     """
     Exif SubjectDistanceRange
     
     Used by
     :attr:`ImageMetadata.subjectDistanceCategory<_sqla.ImageMetadata.subjectDistanceCategory>`
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
     """                                                     # noqa: E501
     UNKNOWN         = 0
     MACRO           = 1
@@ -149,31 +177,65 @@ class ExifSubjectDistanceRange(IntEnum):
     DISTANT_VIEW    = 3
 
 
-class ExifFlashReturn(IntEnum):
-    """Exif Flash Return (part of :class:`ExifFlash` Tag)"""
+_exif_flash_return_str_value = [
+    'No detection function',
+    '(reserved)',
+    'Return not detected',
+    'Return detected'
+]
+
+
+class ExifFlashReturn(ExifEnum):
+    """
+    Exif Flash Return (part of :class:`ExifFlash` Tag)
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
+    """
     NO_STROBE_RETURN_DETECTION_FUNCTION = 0
     RESERVED                            = 1
     STROBE_RETURN_LIGHT_DETECTED        = 2
     STROBE_RETURN_LIGHT_NOT_DETECTED    = 3
 
+    def __str__(self) -> str:
+        return _exif_flash_return_str_value[int(self)]
 
-class ExifFlashMode(IntEnum):
-    """Exif Flash Mode (part of :class:`ExifFlash` Tag)"""
+
+_exif_flash_mode_str_value = ['Unknown', 'On', 'Off', 'Auto']
+
+class ExifFlashMode(ExifEnum):
+    """
+    Exif Flash Mode (part of :class:`ExifFlash` Tag)
+    
+    .. versionchanged:: 0.3.0
+        More readable string representation
+    """
     UNKNOWN                         = 0
     COMPULSORY_FLASH_FIRING         = 1
     COMPULSORY_FLASH_SUPPRESSION    = 2
     AUTO_MODE                       = 3
+    
+    def __str__(self) -> str:
+        return _exif_flash_mode_str_value[int(self)]
 
 
 class ExifFlash:
     """
     Exif Flash Tag
     
-    Used by :attr:`ImageMetadata.flash<_sqla.ImageMetadata.flash>`
+    Used by :attr:`ImageMetadata.flash<_sqla.ImageMetadata.flash>`. To get the
+    numeric value of the flash tag, use ``int(flash_tag)``
     
     Args:
         value:  The :class:`int` value of flash tag, or a :class:`dict`
                 containing the flash information.
+    
+    .. versionchanged:: 0.3.0
+        *   For more consistency with the Exif standard, ``flash_function`` has been
+            replaced by ``no_flash_function`` (its logical negation). The numeric
+            representation has not changed and always conformed to the standard.
+        *   ``__repr__`` shows the fields' names
+        *   More readable ``__str__``
     """
     def __init__(
         self,
@@ -189,17 +251,52 @@ class ExifFlash:
                 self._value |= (value['flash_return'] << 1)
             if 'flash_mode' in value and value['flash_mode']:
                 self._value |= (value['flash_mode'] << 3)
-            if 'flash_function' in value and not value['flash_function']:
+            if 'no_flash_function' in value and value['no_flash_function']:
                 self._value |= 32
             if 'red_eye_reduction' in value and value['red_eye_reduction']:
                 self._value |= 64
         else:
             raise TypeError('Flash constructor Argument must be int or dict')
     
-    def __int__(self):
+    def __repr__(self) -> str:
+        desc = []
+        if self.flash_fired:
+            desc.append('FIRED')
+        if self.flash_mode:
+            desc.append(self.flash_mode.name)
+        if self.flash_return:
+            desc.append(self.flash_return.name)
+        if self.red_eye_reduction:
+            desc.append('RED_EYE_REDUCTION')
+        if self.no_flash_function:
+            desc.append('NO_FUNCTION')
+        return '<{}({})>'.format(
+            self.__class__.__name__,
+            ','.join(desc)
+        )
+    
+    def __str__(self) -> str:
+        desc = []
+        if self.flash_mode:
+            desc.append(str(self.flash_mode))
+        if self.flash_fired:
+            desc.append('Fired')
+        else:
+            desc.append('Did not fire')
+        if self.red_eye_reduction:
+            desc.append('Red-eye reduction')
+        if self.flash_return:
+            desc.append(str(self.flash_return))
+        if self.no_flash_function:
+            desc.append('No flash function')
+        return ', '.join(desc)
+    
+    def __int__(self) -> int:
         return self._value
     
-    def __eq__(self, other: Any):
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(other, ExifFlash):
+            return int(other) == self._value
         return other == self._value
     
     @property
@@ -218,9 +315,9 @@ class ExifFlash:
         return ExifFlashMode((self._value >> 3) & 3)
     
     @property
-    def flash_function(self) -> bool:
+    def no_flash_function(self) -> bool:
         """Flash function supported?"""
-        return not bool((self._value >> 5) & 1)
+        return bool((self._value >> 5) & 1)
     
     @property
     def red_eye_reduction(self) -> bool:
